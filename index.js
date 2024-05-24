feadc8d79df28290219c472e5dd82d473b31f586
MuniasamyKumar <muniasamy1909@gmail.com>
2024-05-24 16:37:42 +0530

    Test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Server is Up and Running...",
-    data: "Hey",
+    data: "Hey Prabu...",
   });
 });
 
